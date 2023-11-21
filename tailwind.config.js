51fe4a82eb2560a4f24acdfcf245f7efd8f05cf6
neytor <ortegajoseweb02@gmail.com>
2023-11-21 19:00:41 -0300

    Bases y config

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        text: "#FEF9EC",
+        background: "#10100A",
+        primary: "#DDB400",
+        secondary: "#2A2822",
+        accent: "BDA642",
+      },
+      fontFamily: {
+        body: ["Laila", "serif"],
+        heading: ["Dancing Script", "serif"],
+      },
+    },
+  },
+};
