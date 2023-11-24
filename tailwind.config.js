712abb3d76be59458816f9b31d871123a87d77e1
neytor <ortegajoseweb02@gmail.com>
2023-11-23 21:27:38 -0300

    Home Page mobile

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,11 +6,15 @@ tailwind.config = {
         background: "#10100A",
         primary: "#DDB400",
         secondary: "#2A2822",
-        accent: "BDA642",
+        accent: "#BDA642",
+        inputs: "#53514E",
       },
       fontFamily: {
         body: ["Laila", "serif"],
         heading: ["Dancing Script", "serif"],
+      },
+      backgroundImage: {
+        hero: "url('./img/cortes/woman_blonde_with_new_hair_sty (2).jpeg')",
       },
     },
   },
